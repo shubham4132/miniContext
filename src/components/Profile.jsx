59668fe910850d8bb68b553d03a44e88e367ef70
shubham4132 <shubhamg3122134@gmail.com>
2024-03-12 17:41:47 +0530

    learning basic context api

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import UserContext from "../Context/UserContext";
+import { useContext } from "react";
+
+function Profile() {
+  const { user } = useContext(UserContext);
+
+  if (!user) return <div>Please login</div>;
+  return <div>Welcome {user.username}</div>;
+}
+
+export default Profile;
